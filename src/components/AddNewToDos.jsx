8bce565c17b6f179884e4c1eb18f68e906ef5d68
Atlessc <107712050+Atlessc@users.noreply.github.com>
2023-02-06 17:58:22 -0800

    i stopped at part 5 @12:25

--- src/components/AddNewToDos.jsx
+++ src/components/AddNewToDos.jsx
@@ -3,20 +3,21 @@ import Modal from "./Modal";
 
 function AddNewToDos() {
     const [showModal, setShowModal] = useState(false);
+
     return (
         <div className='AddNewToDo'>
-            <button className="btn" onClick={() => setShowModal(true)}>
-                <div>
+            <div className="btn">
+                <button onClick={() => setShowModal(true)}>
                     + New To Do
-                </div>
-            </button>
-            <Modal showModal={showModal} setShowModal={setShowModal}>
-                <div>
-                    hello i work
-                </div>
-            </Modal>
+                </button>
+            </div>
+                <Modal showModal={showModal} setShowModal={setShowModal}>
+                    <div>
+                        hello i work
+                    </div>
+                </Modal>
             </div>
     );
-    };
+};
 
 export default AddNewToDos;
